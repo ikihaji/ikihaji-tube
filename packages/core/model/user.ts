01c83e003c799e4b4dcca5cf7c6589eb7b0cb750
shio <85730998+dino3616@users.noreply.github.com>
2024-11-02 00:48:06 +0900

    feat: ✨ (packages) core モジュールを作成

---
+++ packages/core/model/user.ts
@@ -0,0 +1,6 @@
+import type { ViewingHistory } from './view-history';
+
+export type User = {
+  id: string;
+  viewingHistory: ViewingHistory;
+};
---
+++ packages/core/util/get-base-url.ts
@@ -0,0 +1,42 @@
+import { match } from 'ts-pattern';
+
+type GetBaseUrlConfig = {
+  app: 'api';
+};
+
+const getApiBaseUrl = () => {
+  // HACK: Due to unknown causes, URLs as types cannot be uniquely resolved,
+  // so following is described in the form of a Immediately Invoked Function Expression.
+  const baseUrl = (() => {
+    if (process.env['IKIHAJI_TUBE_API_ENDPOINT']) {
+      return new URL(process.env['IKIHAJI_TUBE_API_ENDPOINT']);
+    }
+    if (process.env['NODE_ENV'] === 'production') {
+      return new URL('https://ikihaji-tube-api.up.railway.app/api');
+    }
+
+    return new URL(`http://localhost:${process.env['PORT'] || 4000}`);
+  })();
+
+  return baseUrl;
+};
+
+/**
+ * Get the base URL of the app.
+ * @param app The app from which to get the base URL.
+ * @returns The base URL of the app with trailing slash removed.
+ * @example
+ * ```ts
+ * const baseUrl = getBaseUrl({ app: 'api' });
+ * // => https://ikihaji-tube-api.up.railway.app/api
+ * ```
+ */
+export const getBaseUrl = ({ app }: GetBaseUrlConfig) => {
+  const baseUrl = match(app)
+    .with('api', () => getApiBaseUrl())
+    .exhaustive();
+
+  baseUrl.pathname = baseUrl.pathname.replace(/\/$/, '');
+
+  return baseUrl;
+};
