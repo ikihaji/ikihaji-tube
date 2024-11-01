5d6a1fac31ed95a31e1bf7c5ad715ec40cbcac04
shio <85730998+dino3616@users.noreply.github.com>
2024-11-02 05:17:53 +0900

    feat: ✨ (apps) scrobble-chrome-extension を実装

---
+++ apps/scrobble-chrome-extension/src/content.ts
@@ -0,0 +1,33 @@
+import type { Video } from '@ikihaji-tube/core/model';
+
+(() => {
+  const handleUrlChange = () => {
+    if (location.pathname !== '/watch') {
+      return;
+    }
+
+    setTimeout(() => {
+      const videoId = new URLSearchParams(location.search).get('v');
+      const videoTitle = document.title.replace(' - YouTube', '');
+      const videoThumbnailUrl = `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
+
+      chrome.runtime.sendMessage({
+        action: 'push-video-to-viewing-history',
+        data: {
+          id: videoId ?? '',
+          title: videoTitle,
+          thumbnailUrl: videoThumbnailUrl ?? '',
+        } satisfies Video,
+      });
+    }, 1000);
+  };
+
+  let lastUrl: string = location.href;
+  new MutationObserver(() => {
+    const url: string = location.href;
+    if (url !== lastUrl) {
+      lastUrl = url;
+      handleUrlChange();
+    }
+  }).observe(document, { subtree: true, childList: true });
+})();
