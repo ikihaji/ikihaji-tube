4fd49d6a9e8dbf812751242bae88a0069980c1ff
shio <85730998+dino3616@users.noreply.github.com>
2024-11-03 13:49:05 +0900

    oroka

--- apps/scrobble-chrome-extension/src/content.ts
+++ apps/scrobble-chrome-extension/src/content.ts
@@ -8,7 +8,10 @@ import type { Video } from '@ikihaji-tube/core/model';
 
     setTimeout(() => {
       const videoId = new URLSearchParams(location.search).get('v');
-      const videoTitle = document.querySelector('h1.ytd-watch-metadata')?.textContent;
+      const videoTitle = document
+        .querySelector('h1.ytd-video-primary-info-renderer')
+        ?.textContent?.replace(/\r?\n/g, '')
+        .replace(/^\s+|\s+$/g, '');
       const videoThumbnailUrl = `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
 
       chrome.runtime.sendMessage({
